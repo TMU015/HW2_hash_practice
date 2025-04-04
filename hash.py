03a0600fb4bdcb3875c028bcf78e9f3652ff162b
TMU015 <b117112015@tmu.edu.tw>
2025-04-04 18:42:27 +0800

    修改

--- hash.py
+++ hash.py
@@ -11,17 +11,17 @@ for word in words:
     else:
         word_counts[word] = 1
 
-unique_word_count = 0
+unique_word_counts = 0
 for word in word_counts:
-    unique_word_count += 1
+    unique_word_counts += 1
 
-print("不重複的英文字數量:", unique_word_count)
+print("不重複的英文字數量:", unique_word_counts)
 print("每個英文字的出現次數:")
 print(dict(word_counts))
 
-words = list(word_counts.keys())
-counts = list(word_counts.values())
-
+sorted_items = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+sorted_words = [item[0] for item in sorted_items]
+sorted_counts = [item[1] for item in sorted_items]
 plt.figure(figsize=(10, 6))
-plt.bar(words, counts)
-plt.show()
+plt.bar(sorted_words, sorted_counts)
+plt.show()  
